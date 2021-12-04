4c39ccb56e77abd1f8e0a73cdaca48aaf322d500
SHEIKH MUHAMMAD SAAD <95440412+Muezism@users.noreply.github.com>
2021-12-04 23:36:07 +0500

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,6 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hello World! My Name is Abduk Muiz And i am Student of Fast National University");
     }
 }
